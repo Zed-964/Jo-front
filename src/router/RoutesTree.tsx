3227187ddf9d88f1ddba35c77c3d691cf9a3015e
Zed-964 <enzotechnic@gmail.com>
2024-05-31 00:01:36 +0200

    Updated Paths Updated paths to be resolved correctly

--- src/router/RoutesTree.tsx
+++ src/router/RoutesTree.tsx
@@ -1,9 +1,9 @@
 import { Route, Routes } from "react-router-dom";
-import About from "../pages/About";
-import Basket from "../pages/Basket";
-import ErrorPage from "../pages/ErrorPage";
-import Home from "../pages/Home";
-import Login from "../pages/Login";
+import About from "../pages/About/About";
+import Basket from "../pages/Basket/Basket";
+import ErrorPage from "../pages/ErrorPage/ErrorPage";
+import Home from "../pages/Home/Home";
+import Login from "../pages/Login/Login";
 
 
 
