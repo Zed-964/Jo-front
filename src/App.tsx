9bf8eba53f77d2dd46933bda6c39fbbaca21961b
Zed-964 <enzotechnic@gmail.com>
2024-05-31 00:01:39 +0200

    Fix: Removed deprecated imports.

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,6 @@ import RoutesTree from "./router/RoutesTree";
 import { BrowserRouter as Router } from "react-router-dom";
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
-import CurrentLanguageProvider from "./providers/CurrentLanguageProvider";
-// import PageContentProvider from "./providers/PageContentProvider";
 import BasketContentProvider from "./providers/BasketContentProvider";
 import UserInfosProvider from "./providers/UserInfosProvider";
 import UserStatusProvider from "./providers/UserStatusProvider";
@@ -12,7 +10,6 @@ import UserStatusProvider from "./providers/UserStatusProvider";
 function App() {
     return (
         <div className="main">
-            <CurrentLanguageProvider>
                 <UserInfosProvider>
                     <UserStatusProvider>
                         <BasketContentProvider>
@@ -24,7 +21,6 @@ function App() {
                         </BasketContentProvider>
                     </UserStatusProvider>
                 </UserInfosProvider>
-            </CurrentLanguageProvider>
         </div>
     );
 }
