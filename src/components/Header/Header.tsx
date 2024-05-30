9cf21a330bdafe7defc26e3bed4cb20733a23b96
Zed-964 <enzotechnic@gmail.com>
2024-05-31 00:01:37 +0200

    Header div has been changed for an Header tag Way better for SEO and page structure

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,9 +2,9 @@ import './Header.css'
 
 const Header = () => {
     return (
-        <div className="header">
+        <header className="header">
             <h1>Header</h1>
-        </div>
+        </header>
     );
 };
 
