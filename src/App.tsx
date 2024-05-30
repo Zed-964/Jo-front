bc16c1cabacbcd70d1178161e7f3af7667ee7110
Zed-964 <enzotechnic@gmail.com>
2024-05-31 00:01:36 +0200

    Added Router support as expected Included RoutesTree and global Header / Footer to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,19 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./style/App.css";
 import RoutesTree from "./router/RoutesTree";
+import { BrowserRouter as Router } from "react-router-dom";
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
 
 function App() {
-    const [count, setCount] = useState(0);
+    console.log("hello world");
 
     return (
         <div className="main">
-            <RoutesTree />
+            <Router>
+                <Header />
+                <RoutesTree />
+                <Footer />
+            </Router>
         </div>
     );
 }
