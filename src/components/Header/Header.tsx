65f204d381134140ffc99d80d0cba7575c8e72a9
Zed-964 <enzotechnic@gmail.com>
2024-05-31 00:01:37 +0200

    Header edited and continued Language selector now works as expected

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,9 +1,36 @@
-import './Header.css'
+import "./Header.css";
+import Logo from "../Logo/Logo";
+import LanguageSelector from "../LanguageSelector/LanguageSelector";
+import ProfileSelector from "../ProfileSelector/ProfileSelector";
+import { useState } from "react";
 
 const Header = () => {
+    const logoHeight: string = "150px";
+    const logoWidth: string =
+        Math.round(0.878787878787 * parseInt(logoHeight)) + "px";
+
+    const [isConnected, setIsConnected] = useState(false);
+
     return (
         <header className="header">
-            <h1>Header</h1>
+            <div className="language__selector__wrapper">
+                <LanguageSelector />
+            </div>
+            <button
+                className="header__logo__button"
+                onClick={() => (window.location.href = "/")}
+            >
+                <Logo
+                    src="/assets/img/logos/olympics-logo.svg"
+                    alt="Olympics Logo"
+                    width={logoWidth}
+                    height={logoHeight}
+                    classes="header__logo"
+                />
+            </button>
+            <div className="profile__wrapper">
+                <ProfileSelector isConnected/>
+            </div>
         </header>
     );
 };
