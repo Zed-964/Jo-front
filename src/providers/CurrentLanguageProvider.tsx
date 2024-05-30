6a32e3dc02448dc8fd876c16e71c99a0e4316914
Zed-964 <enzotechnic@gmail.com>
2024-05-31 00:01:37 +0200

    Simplified language provider

--- src/providers/CurrentLanguageProvider.tsx
+++ src/providers/CurrentLanguageProvider.tsx
@@ -6,15 +6,15 @@ export const CurrentLanguageContext = createContext({
 });
 
 const CurrentLanguageProvider = ({ children }: { children: ReactNode }) => {
+    const locale = navigator.language.split("-")[0]; // Gets the language from the browser settings
+
     const [currentLanguage, setCurrentLanguage] = useState(
-        localStorage.getItem("currentLanguage") || "en"
+        localStorage.getItem("currentLanguage") ?? locale ?? "en"// Default language is the local storage value if it exists, if it doesn't, it takes the locale language from navigator, and if not recognized, it will default to English 
     );
-    const locale = navigator.language;
 
     useEffect(() => {
         localStorage.setItem("currentLanguage", currentLanguage);
     }, [currentLanguage]);
-
 
     return (
         <CurrentLanguageContext.Provider
