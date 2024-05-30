88e1af8639315196843d2df073f4d8ae97c3f5e6
Zed-964 <enzotechnic@gmail.com>
2024-05-31 00:01:36 +0200

    Pages init Pages initialisation with dummy values

---
+++ src/pages/ErrorPage/ErrorPage.tsx
@@ -0,0 +1,11 @@
+import './ErrorPage.css'
+
+const ErrorPage = () => {
+    return (
+        <div className="error">
+            <h1>Error</h1>
+        </div>
+    );
+};
+
+export default ErrorPage;
