6ff219e296d9f322c5f2995a9cb09bffd5fb5e9b
Zed-964 <enzotechnic@gmail.com>
2024-05-31 00:01:37 +0200

    Added support for Providers Providers now wrap the app

--- src/App.tsx
+++ src/App.tsx
@@ -3,17 +3,23 @@ import RoutesTree from "./router/RoutesTree";
 import { BrowserRouter as Router } from "react-router-dom";
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
+import CurrentLanguageProvider from "./providers/CurrentLanguageProvider";
+import PageContentProvider from "./providers/PageContentProvider";
 
 function App() {
     console.log("hello world");
 
     return (
         <div className="main">
-            <Router>
-                <Header />
-                <RoutesTree />
-                <Footer />
-            </Router>
+            <CurrentLanguageProvider>
+                <PageContentProvider>
+                    <Router>
+                        <Header />
+                        <RoutesTree />
+                        <Footer />
+                    </Router>
+                </PageContentProvider>
+            </CurrentLanguageProvider>
         </div>
     );
 }
